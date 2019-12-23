34824b1704591330e66d24f81e4a84f60b2b2d43
Bogdankozlovskiy <bkozlovskiy@bk.ru>
2019-12-23 17:21:56 +0300

    add mdoel.py

--- demo/views/frontend.py
+++ demo/views/frontend.py
@@ -1,6 +1,9 @@
+from sqlalchemy import select
 import aiohttp
 from aiohttp_jinja2 import template
 import logging
+from sqlalchemy.sql import text
+from .. import model
 
 
 @template('index.html')
@@ -9,3 +12,9 @@ async def index(request):
 	site_name = request.app['config'].get('site_name')
 	return {'site_name':site_name}
 	return aiohttp.web.Response(text='OK')
+
+async def post(request):
+	async with request.app['db'].acquire() as conn:
+		query = select([model.post])
+		result = await conn.fetch(query)
+	return aiohttp.web.Response(body = str(result))
